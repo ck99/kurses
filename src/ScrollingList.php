d68532244fa3fb01642ab0acd8426f3269d01d47
Ciaran Kelly <ciaran.kelly@gmail.com>
2014-07-13 23:02:43 +0100

    Fix list scroll a bit

--- src/ScrollingList.php
+++ src/ScrollingList.php
@@ -21,7 +21,7 @@ class ScrollingList extends Panel {
     }
 
     public function addLine($line) {
-        $line = substr(chunk_split($line, $this->cols-6, "\r\n"), 0, -2);
+        $line = substr(chunk_split($line, $this->cols-8, "\r\n"), 0, -2);
         $chunks = explode("\r\n", $line);
         foreach($chunks as $i => $chunk) {
             if($i > 0) {
@@ -35,7 +35,7 @@ class ScrollingList extends Panel {
     protected function scrollIfNecessary()
     {
         while(
-            (count($this->lines) == ($this->getMaxY()-2)) ||
+            (count($this->lines) > ($this->getMaxY()-3)) ||
             ($this->maxLines > 0 && count($this->lines) == $this->maxLines)
         ) {
             array_shift($this->lines);
